321d0f92f140690c2b251479d8667db9dd78b2f0
Jack2-1 <2960853985@qq.com>
2023-04-07 13:40:42 +0800

    add gittest.java

---
+++ src/main/java/com/jyu/git/gittest.java
@@ -0,0 +1,11 @@
+package com.jyu.git;
+
+public class gittest {
+
+    public static void main(String[] args) {
+        System.out.println("hello , git");
+        System.out.println("hello , git2");
+        System.out.println("hello , git3");
+        System.out.println("hello , git4");
+    }
+}
